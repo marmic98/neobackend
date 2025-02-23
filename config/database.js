f8dd97ae654b3dc4d52766453c18db23bc173e14
marmic98 <michelmartino98@gmail.com>
2025-02-23 16:16:39 +0100

    update db psw

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('neo_db', 'root', '1111', {
+const sequelize = new Sequelize('neo_db', 'root', 'NuovaEO1!', {
     host: 'localhost',
     dialect: 'mysql'
 });
